dedb910db3a96ee7a7c8952c03eda5271405fe75
Kushal Bhalgat <100193232+KushalBhalgat@users.noreply.github.com>
2023-08-25 14:07:18 +0530

    Update AudioSpectrum.cs

--- ProjectFiles/Assets/Scripts/AudioSpectrum.cs
+++ ProjectFiles/Assets/Scripts/AudioSpectrum.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioSpectrum : MonoBehaviour {
-    private float[] m_audioSpectrum;
+    private float[] spectrum;
     public static float spectrumValue {get; private set;}
-
-	private void Update(){
-        AudioListener.GetSpectrumData(m_audioSpectrum, 0, FFTWindow.Hamming);
-        if (m_audioSpectrum != null && m_audioSpectrum.Length > 0){
-            spectrumValue = m_audioSpectrum[0] * 100;
-        }
-    }
-
+    
     private void Start(){
-        m_audioSpectrum = new float[128];
+        spectrum = new float[128];
+    }
+    private void Update(){
+        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Hamming);
+        if (spectrum != null && spectrum.Length > 0){
+            spectrumValue = spectrum[0] * 100;
+        }
     }
 }
